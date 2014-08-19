cdd50855135332d508458d85688c717617a73f9b
anderslemke <mail@anderslemke.dk>
2014-08-19 10:01:26 +0200

    Nice intructions

--- install.sh
+++ install.sh
@@ -2,3 +2,8 @@ ln -is ~/dotfiles/vimrc ~/.vimrc
 ln -is ~/dotfiles/vim ~/.vim
 ln -is ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -is ~/dotfiles/.gitconfig ~/.gitconfig
+echo '==============================='
+echo 'Make sure to add'
+echo 'source ~/dotfiles/.bash_profile'
+echo 'to your ~/.bash_profile'
+echo '==============================='
