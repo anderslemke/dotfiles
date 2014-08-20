869224625641b006328695d45263ceaf44b961ec
anderslemke <mail@anderslemke.dk>
2014-08-20 10:41:28 +0200

    Install intructions

--- install.sh
+++ install.sh
@@ -7,3 +7,15 @@ echo 'Make sure to add'
 echo 'source ~/dotfiles/.bash_profile'
 echo 'to your ~/.bash_profile'
 echo '==============================='
+
+
+echo '==============================='
+echo 'Make sure to add the folliwing to'
+echo 'vi /Applications/GitHub.app/Contents/Resources/git/share/git-core/templates/info/exclude'
+echo ''
+echo 'doc/tag'
+echo 'doc/tag-ja'
+echo ''
+echo 'This is your current file:'
+cat /Applications/GitHub.app/Contents/Resources/git/share/git-core/templates/info/exclude
+echo '==============================='
