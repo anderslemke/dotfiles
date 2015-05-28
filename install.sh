6538e011d427a5752cd69025a1d4571583ea4177
Anders Lemke <mail@anderslemke.dk>
2015-05-28 02:25:34 -0700

    Also update when installing

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ ln -Fnis ~/dotfiles/.tmuxinator ~/.tmuxinator
 ln -is ~/dotfiles/.editrc ~/.editrc
 ln -is ~/dotfiles/.inputrc ~/.inputrc
 ln -is ~/dotfiles/.tigrc ~/.tigrc
-vim +PlugClean +PlugInstall +qall
+vim +PlugClean +PlugInstall +PlugUpdate +qall
 ~/dotfiles/platforms/install.rb
 echo '==============================='
 echo 'Make sure to add something like'
